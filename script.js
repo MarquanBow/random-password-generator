5b8c4d85dcd06b02b122d220be33c86325fea8a6
Marquan <marquan.bowman@outlook.com>
2024-07-06 18:54:56 -0400

    add length and options for number and symbols

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 const passwordBox = document.getElementById("password");
+const lengthInput = document.getElementById("length");
+const includeNumbersCheckbox = document.getElementById("includeNumbers");
+const includeSymbolsCheckbox = document.getElementById("includeSymbols");
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "@#$%^&*()_+~|}{[]></-=! ";
 
-function generate(event) {
-    event.preventDefault();
 
-    const form = document.getElementById("passwordForm");
-    const length = parseInt(form.length.value);
-    const includeNumbers = form.includeNumbers.checked;
-    const includeSymbols = form.includeSymbols.checked;
-
-    if (length < 8) {
-        alert("Password length should be at least 8 characters.");
-        return;
-    }
-
+function generate(){
+    const length = parseInt(lengthInput.value);
+    const includeNumbers = includeNumbersCheckbox.checked;
+    const includeSymbols = includeSymbolsCheckbox.checked;
     let allChars = upperCase + lowerCase;
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-
     if (includeNumbers) {
         allChars += numbers;
         password += numbers[Math.floor(Math.random() * numbers.length)];
@@ -32,15 +26,13 @@ function generate(event) {
         allChars += symbols;
         password += symbols[Math.floor(Math.random() * symbols.length)];
     }
-
-    while (password.length < length) {
+    while(length > password.length){
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
-
     passwordBox.value = password;
 }
 
 function copyPassword() {
     passwordBox.select();
     navigator.clipboard.writeText(passwordBox.value);
-}
+  }
